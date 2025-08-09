27ad320fac3991472dfabe0963d04dc9268d20f9
Fichusgg <fichusgg@gmail.com>
2025-08-09 20:39:49 -0300

    apikey

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -6,13 +6,15 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // IMPORTANT: Use environment variables to hide these keys in production!
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyB5u6lJbNCT_nBZNM1GRDsmuAvfDGcOMgM",
+  authDomain: "sitebuilder-7c509.firebaseapp.com",
+  projectId: "sitebuilder-7c509",
+  storageBucket: "sitebuilder-7c509.firebasestorage.app",
+  messagingSenderId: "281206894740",
+  appId: "1:281206894740:web:746302365e48dc466a8481",
+  measurementId: "G-04VWCS165E"
 };
 
 // Initialize Firebase
